18829af72c1d3558a0ffb901c1df6f0da9a76056
vlad <vladbeklenyshchev@gmail.com>
2018-06-16 01:20:36 +0300

    Problem "Two Sum"

---
+++ LeetCode/problem-1-two-sum/two-sum.c
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> tmp;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for(int j = i+1; j < nums.size(); j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    tmp = vector<int>({i,j}); 
+                    return tmp;
+                }
+            }
+        }
+        return tmp;
+    }
+};
