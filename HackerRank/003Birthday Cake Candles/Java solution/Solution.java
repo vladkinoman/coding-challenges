6b9d78c49f508104f03835df64f7be25603e5162
vlad <vladbeklenyshchev@gmail.com>
2018-07-15 20:44:45 +0300

    Java solution of problem #003 Birthday Cake Candles

--- HackerRank/003Birthday Cake Candles/Java solution/Solution.java
+++ HackerRank/003Birthday Cake Candles/Java solution/Solution.java
@@ -10,14 +10,34 @@ public class Solution {
 
     // Complete the birthdayCakeCandles function below.
     static int birthdayCakeCandles(int[] ar) {
+    	if(ar.length < 1 || ar.length > 100000)
+			throw new NegativeArraySizeException(
+					"Count of candles must be greater 1 <= height <=10^5");
+		
+		for (int i = 0; i < ar.length; ++i)
+			if(ar[i] < 1 || ar[i] > 10000000)
+				throw new IllegalArgumentException(
+					"Height of candles must be greater 1 <= height <=10^7");
+    	
+    	int max = ar[0];
+    	int countCandles = 0;
+    	for (int i = 0; i < ar.length ; i++)
+    	{
+    		if(max < ar[i])
+    		{
+    			max = ar[i];
+    			countCandles = 0;	
+    		} 
+    		if(max == ar[i]) countCandles++;
+    	}	
 
-
+    	return countCandles;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("out.txt"/*System.getenv("OUTPUT_PATH")*/));
 
         int arCount = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
