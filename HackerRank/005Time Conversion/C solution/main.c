a7ce6641d1ae134e170c5dbfc6e526724d373745
vlad <vladbeklenyshchev@gmail.com>
2018-07-16 22:23:20 +0300

    First part of C solution of problem #005 Time Conversion

--- HackerRank/005Time Conversion/C solution/main.c
+++ HackerRank/005Time Conversion/C solution/main.c
@@ -7,7 +7,21 @@
 #include <string.h>
 
 char* readline();
-
+char* itoa_zero(int num, char* s)
+{
+	if(num < 10)
+	{
+		s[0] = '0';
+		s[1] = '0' + num;	
+	}
+	else
+	{
+		s[0] = '0'+ (num / 10);
+		s[1] = '0'+ (num % 10);
+	}
+	s[2] = '\0';
+	return s;
+}
 /*
  * Complete the timeConversion function below.
  */
@@ -24,23 +38,48 @@ char* readline();
  *
  */
 char* timeConversion(char* s) {
-    /*
-     * Write your code here.
-     */
+	char * input_str = malloc(10*sizeof(char));
+	strcpy(input_str, s);
+	char * hh = strtok(s, ":");
+	char * other_part = strtok(NULL, "AP");
+	char * half = strpbrk(input_str, "AP");
+	char * str = malloc(10);
+	if     (strcmp(hh, "12") == 0 && strcmp(half, "AM") == 0)
+		str = "00:00:00";
+	else if(strcmp(hh, "12") == 0 && strcmp(half, "PM") == 0
+		|| strcmp(hh, "12") != 0 && strcmp(half, "AM") == 0) 
+		str = input_str;
+	else if(strcmp(hh, "12") != 0 && strcmp(half, "PM") == 0)
+	{
+		int ihh = atoi(hh);
+		ihh += 12;
+		char * shh = malloc(10 * sizeof(char));
+		shh = itoa_zero(ihh, shh);
+		str = strcat(str, strcat(shh, strcat(":", other_part)));
+	}
+	return str;
+	//printf("%s\n%s\n%s\n%s\n", hh, mm, ss, half);
+	//printf("%s\n", input_str);
 
+	/*if(strcmp(hh, "12") == 0 
+		&& strcmp(mm, "00") == 0
+		&& strcmp(ss, "00") == 0)
+	*/
 }
+
+
 
 int main()
 {
-    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    //FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
 
     char* s = readline();
 
     char* result = timeConversion(s);
 
-    fprintf(fptr, "%s\n", result);
+    fprintf(stdout/*fptr*/, "%s\n", result);
 
-    fclose(fptr);
+    //fclose(fptr);
 
     return 0;
 }
