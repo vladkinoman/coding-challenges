5fd6a4065f044b547ab49a40c2552ba591b3a7f5
vlad <vladbeklenyshchev@gmail.com>
2018-07-17 15:32:30 +0300

    Revert "C solution for TimeConversion"
    This reverts commit 8c07c5f54239a592e09d97aa6b3d7ef19f3d2edc.

--- HackerRank/006Grading Students/C solution/main.c
+++ HackerRank/006Grading Students/C solution/main.c
@@ -22,38 +22,15 @@ char* readline();
  *
  */
 int* gradingStudents(int grades_count, int* grades, int* result_count) {
-    if(grades_count < 1 || grades_count > 60)
-        exit(1);
-    for(int i = 0; i < grades_count; i++)
-        if(*(grades + i) < 0 || *(grades + i) > 100)
-            exit(1);
-    
-    int * a = malloc(grades_count*sizeof(int));
-    *result_count = grades_count;
+    /*
+     * Write your code here.
+     */
 
-    for(int i = 0; i < grades_count; i++)
-    {
-        if (grades[i] < 38) 
-        {
-            a[i] = grades[i];
-            continue;
-        }
-        int nmultiple;
-        for (nmultiple = grades[i]; ; ++nmultiple)
-            if((nmultiple % 5) == 0)
-                break;
-        if((nmultiple - grades[i]) < 3)
-            a[i] = nmultiple;
-        else
-            a[i] = grades[i];
-        
-    }            
-    return a;
 }
 
 int main()
 {
-    //FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
 
     char* n_endptr;
     char* n_str = readline();
@@ -74,20 +51,19 @@ int main()
     }
 
     int result_count;
-    int grades_count = n;
     int* result = gradingStudents(grades_count, grades, &result_count);
 
     for (int result_itr = 0; result_itr < result_count; result_itr++) {
-        fprintf(stdout/*fptr*/, "%d", result[result_itr]);
+        fprintf(fptr, "%d", result[result_itr]);
 
         if (result_itr != result_count - 1) {
-            fprintf(stdout/*fptr*/, "\n");
+            fprintf(fptr, "\n");
         }
     }
 
-    fprintf(stdout/*fptr*/, "\n");
+    fprintf(fptr, "\n");
 
-    //fclose(fptr);
+    fclose(fptr);
 
     return 0;
 }
