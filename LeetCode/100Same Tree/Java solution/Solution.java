4ed87db395d4e7ecca5a4f73c6221ee645edbd9e
vlad <vladbeklenyshchev@gmail.com>
2018-07-17 20:20:58 +0300

    Java solution for problem 100 Same Tree

--- LeetCode/100Same Tree/Java solution/Solution.java
+++ LeetCode/100Same Tree/Java solution/Solution.java
@@ -9,6 +9,12 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        
+		if(p == null && q == null)
+			return true;
+		if(p != null && q == null || p == null && q != null
+			|| p != null && q != null && p.val != q.val)
+			return false;
+		return isSameTree(p.left, q.left) 
+			&& isSameTree(p.right, q.right);        
     }
 }
