641cf03fe94d1acb3ffd534a779db9ea4948464b
vlad <vladbeklenyshchev@gmail.com>
2018-07-17 20:16:50 +0300

    C solution for problem 100 Same Tree

--- LeetCode/100Same Tree/C solution/main.c
+++ LeetCode/100Same Tree/C solution/main.c
@@ -7,5 +7,11 @@
  * };
  */
 bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
-    
+	if(p == NULL && q == NULL)
+		return true;
+	if(p != NULL && q == NULL || p == NULL && q != NULL
+		|| p != NULL && q != NULL && p->val != q->val)
+		return false;
+	return isSameTree(p->left, q->left) 
+		&& isSameTree(p->right, q->right);    
 }
