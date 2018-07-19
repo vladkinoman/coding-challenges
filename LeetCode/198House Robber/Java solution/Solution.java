c1f91e83172562e712d7633c7fd6bd2447fda4ef
vlad <vladbeklenyshchev@gmail.com>
2018-07-19 14:11:17 +0300

    Java solution for 198 House Robber problem

--- LeetCode/198House Robber/Java solution/Solution.java
+++ LeetCode/198House Robber/Java solution/Solution.java
@@ -1,5 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        
-    }
+        for (int i = 0; i < nums.length; ++i)
+    		if(nums[i] < 0)
+    			throw new IllegalArgumentException("Amount of money must be non-negative.");
+    
+	    int sumFromFirstHouse = 0;
+	    for (int i = 0; i < nums.length; i+=2)
+	    	sumFromFirstHouse += nums[i];
+	    int sumFromSecondHouse = 0;
+	    for (int i = 1; i < nums.length; i+=2)
+	    	sumFromSecondHouse += nums[i];
+	    if(sumFromFirstHouse > sumFromSecondHouse)
+	    	return sumFromFirstHouse;
+	    else
+	    	return sumFromSecondHouse;
 }
