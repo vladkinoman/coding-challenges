4bdc185f6a43377bcfbff58ba96444b091a40b1d
vlad <vladbeklenyshchev@gmail.com>
2018-07-22 17:45:40 +0300

    Removed protein-translation and rna-transcription problems

--- Exercism.io/java/protein-translation/src/main/java/ProteinTranslator.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-class ProteinTranslator {
-
-    List<String> translate(String rnaSequence) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-}
--- Exercism.io/java/rna-transcription/src/main/java/RnaTranscription.java
+++
@@ -1,7 +0,0 @@
-class RnaTranscription {
-
-    String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-}
