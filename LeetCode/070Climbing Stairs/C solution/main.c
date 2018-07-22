2b864306bc44262171831c24cb2fd7e8d547f700
vlad <vladbeklenyshchev@gmail.com>
2018-07-22 22:08:21 +0300

    C solution for 070 Climbing Stairs (21 / 45 test cases passed - brute-force solution with recursion of time O(2^n)

--- LeetCode/070Climbing Stairs/C solution/main.c
+++ LeetCode/070Climbing Stairs/C solution/main.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int climb_Stairs(int i, int n) {
+    if (i > n) return 0;
+    if (i == n) return 1;
+
+    return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+}
+
 int climbStairs(int n) {
-    
+    return climb_Stairs(0, n);
+}
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	scanf("%d", &n);
+	printf("%d", climbStairs(n));
+	return 0;
 }
