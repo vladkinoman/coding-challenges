bb00ccdad36e0b9e44342f83a3b61c36a059453b
vlad <vladbeklenyshchev@gmail.com>
2018-07-22 17:43:28 +0300

    Update solution for Armstrong numbers problem according to the new platform

--- Exercism.io/java/armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
+++ Exercism.io/java/armstrong-numbers/src/test/java/ArmstrongNumbersTest.java
@@ -21,7 +21,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void noTwoDigitArmstrongNumbers() {
         int input = 10;
@@ -29,7 +28,6 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsArmstrongNumber() {
         int input = 153;
@@ -37,7 +35,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void threeDigitNumberIsNotArmstrongNumber() {
         int input = 100;
@@ -45,7 +42,6 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsArmstrongNumber() {
         int input = 9474;
@@ -53,7 +49,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void fourDigitNumberIsNotArmstrongNumber() {
         int input = 9475;
@@ -61,7 +56,6 @@ public class ArmstrongNumbersTest {
         assertFalse(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsArmstrongNumber() {
         int input = 9926315;
@@ -69,7 +63,6 @@ public class ArmstrongNumbersTest {
         assertTrue(armstrongNumbers.isArmstrongNumber(input));
     }
 
-    //@Ignore("Remove to run test")
     @Test
     public void sevenDigitNumberIsNotArmstrongNumber() {
         int input = 9926314;
