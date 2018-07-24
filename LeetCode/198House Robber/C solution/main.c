2a82e7d7dbb62a3fe7c128fb01f02863abe394de
vlad <vladbeklenyshchev@gmail.com>
2018-07-24 15:30:47 +0300

    198 House Robber problem 50 / 69 test cases passed

--- LeetCode/198House Robber/C solution/main.c
+++ LeetCode/198House Robber/C solution/main.c
@@ -7,6 +7,7 @@ struct list_item
     struct list_item * next;
     int value;
     int is_security_activated;
+    int is_skipped;
 };
 
 struct list_item * list_init(struct list_item * head)
@@ -20,6 +21,7 @@ struct list_item * list_add(struct list_item * head, int value)
     struct list_item * tmp = malloc(sizeof(struct list_item));
     tmp->value = value;
     tmp->is_security_activated = 0;
+    tmp->is_skipped = 0;
     tmp->prev = NULL;
     tmp->next = head;
     if(head) head->prev = tmp;
@@ -35,7 +37,7 @@ struct list_item * list_find_max(struct list_item * head)
     while(head != NULL)
     {
         if(head->value > 0 && head->value > max 
-            && !head->is_security_activated){
+            && !head->is_security_activated && !head->is_skipped){
             max = head->value;
             elem_with_max = head;
         }
@@ -45,25 +47,46 @@ struct list_item * list_find_max(struct list_item * head)
 }
 
 int rob(int* nums, int numsSize) {
-    int sum = 0;
     struct list_item * head;
     head = list_init(head);
 
     for (int i = 0; i < numsSize; ++i)
         head = list_add(head, nums[i]);
+    struct list_item * first = head;
 
-    struct list_item * house_to_rob;
-    while((house_to_rob = list_find_max(head)) != NULL)
+    int sum = 0;
+    int ressum = 0;
+    for(int i = 0; i < numsSize; i++, sum = 0)
     {
-        sum += house_to_rob->value;
-        house_to_rob->value = 0;
-        if(house_to_rob->prev)
-            house_to_rob->prev->is_security_activated = 1;
-        if(house_to_rob->next)
-            house_to_rob->next->is_security_activated = 1;
+        struct list_item * house_to_rob;
+        house_to_rob = list_find_max(head);
+        if(house_to_rob) house_to_rob->is_skipped = 1;
+        while(house_to_rob != NULL)
+        {
+            sum += house_to_rob->value;
+            house_to_rob->value = 0;
+            if(house_to_rob->prev)
+                house_to_rob->prev->is_security_activated = 1;
+            if(house_to_rob->next)
+                house_to_rob->next->is_security_activated = 1;
+            house_to_rob = list_find_max(head);
+        }
+        // reset
+        for(int j = numsSize - 1; head != NULL; j--)
+        {
+            head->value = nums[j];
+            head->is_security_activated = 0;
+            if(head->prev)
+                head->prev->is_security_activated = 0;
+            if(head->next)
+                head->next->is_security_activated = 0;
+            head = head->next;
+        }
+        head = first;
+        if(ressum < sum) ressum = sum;
     }
-
-    return sum;
+    
+    return ressum;
 }
 
 int main(int argc, char const *argv[])
