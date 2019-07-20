ee170db1e637af633410c08b1ffc70644e82a310
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-07-20 21:52:01 +0300

    Add HackerRank tasks to code on vacation (note: SavePrisoner isn't work properly in Visual Studio)

---
+++ HackerRank/017Organizing Containers of Balls/C++ solution/OrganizingContainersOfBalls/OrganizingContainersOfBalls/OrganizingContainersOfBalls.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// Complete the organizingContainers function below.
+string organizingContainers(vector<vector<int>> container) {
+
+	return "string from organizingContainers function";
+}
+
+int main()
+{
+	//ofstream fout(getenv("OUTPUT_PATH"));
+
+	int q;
+	cin >> q;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	for (int q_itr = 0; q_itr < q; q_itr++) {
+		int n;
+		cin >> n;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+		vector<vector<int>> container(n);
+		for (int i = 0; i < n; i++) {
+			container[i].resize(n);
+
+			for (int j = 0; j < n; j++) {
+				cin >> container[i][j];
+			}
+
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+
+		string result = organizingContainers(container);
+
+		cout << result << "\n";
+	}
+
+	//fout.close();
+
+	return 0;
+}
