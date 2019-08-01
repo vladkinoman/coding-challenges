8e8a4ad34c14484ccb74df35ac173e8c073c0d91
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-08-01 23:56:19 +0300

    Finishing Climbing The Leaderboard problem, need to complete subtask and to speed up the algorithm

--- HackerRank/015Climbing the Leaderboard/C++ solution/ClimbingTheLeaderboard/ClimbingTheLeaderboard/ClimbingTheLeaderboard.cpp
+++ HackerRank/015Climbing the Leaderboard/C++ solution/ClimbingTheLeaderboard/ClimbingTheLeaderboard/ClimbingTheLeaderboard.cpp
@@ -6,14 +6,59 @@
 
 using namespace std;
 
-
 vector<string> split_string(string);
+
 
 // Complete the climbingLeaderboard function below.
 vector<int> climbingLeaderboard(vector<int> scores, vector<int> alice) {
-	cout << "This is climbingTheLeaderboard function." << endl;
-	vector<int> v;
-	return v;
+	
+	if (!(scores.capacity() >= 1 && scores.capacity() <= 2 * 100000)) // 10^5
+		throw;
+	if (!(alice.capacity() >= 1 && alice.capacity() <= 2 * 100000))  // 10^5
+		throw;
+
+	/*if(count_if(scores.begin(), scores.end(),;
+		throw;*/
+
+	vector<int> leader_ranks(scores.size());
+	vector<int> alice_ranks(alice.size());
+
+	// filling leader_ranks array
+	for (int i = 0, r = 0; i < scores.size(); i++)
+	{
+		if ((i != 0 && scores[i - 1] < scores[i])
+			|| !(scores[i] >= 0 && scores[i] <= 1000000000)) // 10^9
+			throw;
+		if (i != 0 && scores[i] == scores[i - 1]) leader_ranks[i] = r;
+		else leader_ranks[i] = ++r;
+	}
+
+	// main part of algorithm
+	for (int i = 0; i < alice.size(); i++)
+	{
+		if ((i != 0 && alice[i - 1] > alice[i])
+			|| !(alice[i] >= 0 && alice[i] <= 1000000000)) // 10^9
+			throw;
+		for (int j = leader_ranks.size() - 1; j >= 0; j--)
+		{
+			if (alice[i] == scores[j])
+			{
+				alice_ranks[i] = leader_ranks[j];
+				break;
+			}
+			else if (alice[i] < scores[j])
+			{
+				if (j + 1 != leader_ranks.size())
+					alice_ranks[i] = leader_ranks[j + 1];
+				else
+					alice_ranks[i] = leader_ranks[j] + 1;
+				break;
+			}
+			else if (j == 0) alice_ranks[i] = leader_ranks[j];
+		}
+	}
+
+	return alice_ranks;
 }
 
 int main()
