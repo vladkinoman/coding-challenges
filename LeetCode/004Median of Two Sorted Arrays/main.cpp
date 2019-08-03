4a88e41cbe41f4df0fabb033fae49e4cf888e0ff
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-08-03 16:16:46 +0300

    First hard problem on LeetCode was solved

--- LeetCode/004Median of Two Sorted Arrays/main.cpp
+++ LeetCode/004Median of Two Sorted Arrays/main.cpp
@@ -1,39 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 class Solution {
+private:
+    double findMedianSortedArray(vector<int> & nums)
+	{
+		int n = nums.size();
+		return (n % 2 == 0) ? (double)(nums[(double)n / 2 - 1] 
+		+ nums[(double)(n + 2) / 2 - 1]) / 2
+		: nums[round((double)n / 2) - 1];
+	}
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double med1 = 0.0;
-	double med2 = 0.0;
-        for(int i = 0; i < nums1.capacity(); i++)
-        	med1 += nums1.at(i);
-        med1 /= 2.0;
-        for(int i = 0; i < nums2.capacity(); i++)
-        	med2 += nums2.at(i);
-        med2 /= 2.0;
-        return (med1 + med2) / 2.0;
+        int n = nums1.size() + nums2.size();
+		vector<int> merged_nums(n);
+        if(nums1.size() > 0 && nums2.size() > 0)
+        {
+            merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), merged_nums.begin());
+            return findMedianSortedArray(merged_nums);
+        }
+        else if(nums1.size() > 0 && nums2.size() <= 0)
+            return findMedianSortedArray(nums1);
+        else if(nums1.size() <= 0 && nums2.size() > 0)
+            return findMedianSortedArray(nums2);
+        else
+            return 0;
     }
 };
 
 int main()
 {
+	// test case # 1
 	vector<int> nums1;
 	vector<int> nums2;
-	int x = 0;
-	
-	nums1.push_back(2);	
-	nums1.push_back(1);
-	
-	nums2.push_back(4);	
-	nums2.push_back(3);
+		
+	nums2.push_back(1);
 
 	sort(nums1.begin(), nums1.end());
 	sort(nums2.begin(), nums2.end());
 
-	Solution newSolution;
-	cout << newSolution.findMedianSortedArrays(nums1, nums2) << endl;
+	Solution newSolution1;
+	cout << newSolution1.findMedianSortedArrays(nums1, nums2) << endl;
+
+	// test case # 2
+	vector<int> nums21;
+	vector<int> nums22;
+	
+	nums21.push_back(1);	
+	nums21.push_back(3);
+
+	nums22.push_back(2);
+
+	sort(nums21.begin(), nums21.end());
+	Solution newSolution2;
+	cout << newSolution2.findMedianSortedArrays(nums21, nums22) << endl;
 }
