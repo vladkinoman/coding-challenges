050acf5ccfdb16efd1566fee5357b6eafb182984
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-08-03 11:44:05 +0300

    Complete problem # 111

--- LeetCode/111Minimum Depth of Binary Tree/main.cpp
+++ LeetCode/111Minimum Depth of Binary Tree/main.cpp
@@ -12,37 +12,48 @@ struct TreeNode {
 
 class Solution {
 private:
-    int findMinDepth(TreeNode* root, int n)
+    int findMinDepth(TreeNode* root, int depth)
     {
     	int left_min_depth = 0;
     	int right_min_depth = 0;
     	// found a leaf
-    	if(root->left == NULL && root->right == NULL) return n;
-    	else if(root->left != NULL) return findMinDepth(root->left, n + 1);
-    	else if(root->right != NULL) return findMinDepth(root->right, n + 1);
+    	if(!root->left && !root->right) 
+            return depth;
+    	else if(root->left && !root->right) 
+            return findMinDepth(root->left, depth + 1);
+    	else if(!root->left && root->right) 
+            return findMinDepth(root->right, depth + 1);
     	else
     	{
-    		left_min_depth = findMinDepth(root->left, n + 1);
-    		right_min_depth = findMinDepth(root->right, n + 1);
+    		left_min_depth = findMinDepth(root->left, depth + 1);
+    		right_min_depth = findMinDepth(root->right, depth + 1);
     		return left_min_depth < right_min_depth ? left_min_depth : right_min_depth;
     	}
     }
 public:
     int minDepth(TreeNode* root) {
-        int n = 0;
-        if(root == NULL) return n;
-        return findMinDepth(root, n + 1);
+        int depth = 0;
+        if(!root) return depth;
+        return findMinDepth(root, depth + 1);
     }
 };
 
 int main(int argc, char const *argv[])
 {
-	TreeNode * root = new TreeNode(3);
+	// test case #0
+	/* TreeNode * root = new TreeNode(3);
         root->left = new TreeNode(9);
 	root->right = new TreeNode(20);
 	root->right->left = new TreeNode(15);
-	root->right->right = new TreeNode(7);
+	root->right->right = new TreeNode(7);*/
 	
+	// test case #1
+	TreeNode * root = new TreeNode(1);
+    root->left = new TreeNode(2);
+	root->right = new TreeNode(3);
+	root->left->left = new TreeNode(4);
+	root->left->right = new TreeNode(5);
+
 	Solution newSolution;
 	cout << newSolution.minDepth(root) << endl;
 	return 0;
