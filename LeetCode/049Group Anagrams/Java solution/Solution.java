9450f497edebdc62083b4a6a090f681da3d55ff1
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-10-18 23:24:59 +0300

    Add LeetCode solution for Group Anagrams
    TODO: understand this solution :)

--- LeetCode/049Group Anagrams/Java solution/Solution.java
+++ LeetCode/049Group Anagrams/Java solution/Solution.java
@@ -1,5 +1,22 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-		        
+    	// Solution from LeetCode.
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        int[] count = new int[26];
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+            for (char c : s.toCharArray()) count[c - 'a']++;
+
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append('#');
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
     }
 }
