ba33bda9d19945921caeac96e36ae1d19cbe4139
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-10-20 23:51:23 +0300

    Add a quick solution for StrangeCounter problem
    Add a check for t and optimize loops (remove them?)

---
+++ HackerRank/021Strange Counter/Java solution/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the strangeCounter function below.
+    static long strangeCounter(long t) {
+        if (t <= 1 || t >= Math.pow(10, 12))
+            throw new IllegalArgumentException();
+        
+        long tFirstSecOfCycle = 1;
+        long value = 3;
+        while (true) {
+            tFirstSecOfCycle += value;
+            value = value * 2;
+            if (tFirstSecOfCycle >= t) break;
+        }
+        if (t == tFirstSecOfCycle) return value;
+        tFirstSecOfCycle -= value;
+        for (long i = tFirstSecOfCycle; i < t; i++)
+            value--;
+        return value;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
