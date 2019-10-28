0c57bcd5225e243237e1e9e67b5d04f7280616e1
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-10-29 00:56:18 +0200

    Update: 24/25 test cases were passed

--- LeetCode/017-letter-combinations-of-a-phone-number/java/Solution.java
+++ LeetCode/017-letter-combinations-of-a-phone-number/java/Solution.java
@@ -4,14 +4,22 @@ class Solution {
     private final static String[] phone = {"abc", "def", "ghi", "jkl", "mno", "pqrs",
                           "tuv", "wxyz"};
     
-    private String returnCombination(int digit) {
-        for (int i = 0; i < phone[digit].length(); i++) {
-            
+    private int[] recurrentIndexBacktracking(int[] indexArray, int[] iDigits, int i) {
+        if (i == -1) return indexArray;
+        
+        if (indexArray[i] == phone[iDigits[i]].length() - 1) {
+            indexArray[i] = 0;
+            i--;
+            return recurrentIndexBacktracking(indexArray, iDigits, i);
         }
+        
+        indexArray[i]++;
+        
+        return recurrentIndexBacktracking(indexArray, iDigits, -1);
     }
     
     public List<String> letterCombinations(String digits) {
-        
+        if (digits.isEmpty()) return new Vector<String>();
         for (int i = 0; i < digits.length(); i++) {
             int c = Integer.parseInt(String.valueOf(digits.charAt(i)));
             if (c < 2 || c > 9)
@@ -19,24 +27,28 @@ class Solution {
         }
         
         
-        Integer[] iDigits = new Integer [digits.length()];
+        int[] iDigits = new int [digits.length()];
         int numberOfCombinations = 1;
         for (int i = 0; i < digits.length(); i++) {
             int c = Integer.parseInt(String.valueOf(digits.charAt(i)));
-            numberOfCombinations *= c;
-            iDigits[i] = c - 2; // easy access later
+            iDigits[i] = c - 2; // easy access
+            numberOfCombinations *= phone[iDigits[i]].length();
         }
         
         List<String> lOfLetterCombinations = new Vector<>();
+        int[] indexArray = new int [digits.length()];
         for (int i = 0; i < numberOfCombinations; i++) {
+            String s = "";
             
-        }
-        
-        
-        for (int i = 0; i < phone[iDigits[0]].length(); i++) {
-            String s = new String(String.valueOf(phone[iDigits[0]].charAt(i)) 
-                                      + String.valueOf(phone[iDigits[1]].charAt(j)));
-                lOfLetterCombinations.add(s);
+            for (int j = 0; j < digits.length(); j++) {
+                s = new String (s + String.valueOf(phone[iDigits[j]]
+                    .charAt(indexArray[j])));    
+                
+                recurrentIndexBacktracking(indexArray, iDigits, j);
+            }
+            
+            // creating the string containing the letter combination            
+            lOfLetterCombinations.add(s);       
         }
         
         return lOfLetterCombinations;
