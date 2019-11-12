b5fc1e0834d938302972107a68a65eca2d838380
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-11-13 00:03:02 +0200

    Complete the day-0 of the '10 days of statistics' tutorial

---
+++ HackerRank/tutorials/10-days-of-statistics/day-0-weighted-mean/java/Solution.java
@@ -0,0 +1,39 @@
+﻿import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N < 5 || N > 50)
+            throw new IllegalArgumentException();
+        int[] x = new int[N];
+        for (int i = 0; i < N; i++) {
+            x[i] = scanner.nextInt();
+            if (x[i] <= 0 || x[i] > 100)
+                throw new IllegalArgumentException();
+        }
+        int[] w = new int[N];
+        for (int i = 0; i < N; i++) {
+            w[i] = scanner.nextInt();
+            if (w[i] <= 0 || w[i] > 100)
+                throw new IllegalArgumentException();
+        }
+        scanner.close();
+
+        long numerator = 0;
+        long denominator = 0;
+        for (int i = 0; i < N; i++) {
+            numerator += (long) x[i] * w[i];
+            denominator += (long) w[i];
+        }
+
+        System.out.println(Math.round(
+            (double)numerator/denominator * 10) / 10.0);
+    }
+}
+
