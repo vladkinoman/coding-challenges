dc5c2b1d59973bbf4f32d09b95f80ac8571c9ae1
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-11-16 00:24:59 +0200

    Add first solved problem on HackerEarth (CodeMonk)

---
+++ HackerEarth/code-monk/01-basics-of-programming/basics-of-input-output/tutorial-problem/c++/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <stdexcept>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int N;
+	cin >> N;
+	char S[16];
+	cin>> S;
+    if (N < 0 || N > 10)
+        throw std::invalid_argument("Check the number.");
+    if (strlen(S) < 1 || strlen(S) > 15)
+        throw std::invalid_argument("Check the size of the string.");
+											// Reading input from STDIN
+	cout << N * 2 << endl;		// Writing output to STDOUT
+	cout << S << endl;
+	return 0;
+}
