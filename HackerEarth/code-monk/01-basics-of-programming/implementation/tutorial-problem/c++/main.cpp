fb695edc2454aaba0ca5cd7eb7ba5637a5f90001
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-11-20 00:46:42 +0200

    Add solution to the 'Basics of implementation' tutorial problem

---
+++ HackerEarth/code-monk/01-basics-of-programming/implementation/tutorial-problem/c++/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int nums[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] >= '0' && s[i] <= '9')
+            // In ASCII code, the numbers start from 48.
+            nums[(int) s[i] - 48]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        cout << i << " " << nums[i] << endl;        
+    }
+}
