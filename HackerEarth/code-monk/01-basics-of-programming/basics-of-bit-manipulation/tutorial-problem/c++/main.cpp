b582e76c7f6e49abeac870314cd6c5c3abee6c4f
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-11-25 23:44:39 +0200

    Add solution to the tutorial problem of Basics of bit manipulation

---
+++ HackerEarth/code-monk/01-basics-of-programming/basics-of-bit-manipulation/tutorial-problem/c++/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+int count_one(int n)
+{
+    int count = 0;
+    while (n)
+    {
+        n = n & (n - 1);
+        count++;
+    }
+    return count;
+}
+
+int main() {
+	int T;
+	cin >> T;
+	if (T < 1 || T > 1000)
+	    throw invalid_argument("");
+	
+	for (int i = 0; i < T; i++) {
+	    int N;
+        cin >> N;
+        if (N < 1 || N > 1000)
+            throw invalid_argument("");
+	    cout << count_one(N) << endl;
+	}
+	
+	return 0;
+}
