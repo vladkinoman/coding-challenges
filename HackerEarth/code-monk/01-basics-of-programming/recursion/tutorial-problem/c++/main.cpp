2ef705d05586f84a681ace332d629d25e0501f91
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-11-30 23:33:55 +0200

    Add description of the tutorial problem for Recursion topic

---
+++ HackerEarth/code-monk/01-basics-of-programming/recursion/tutorial-problem/c++/main.cpp
@@ -0,0 +1,17 @@
+﻿/*
+// Sample code to perform I/O:
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;										// Reading input from STDIN
+	cout << "Input number is " << num << endl;		// Writing output to STDOUT
+}
+
+// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
+*/
+
+// Write your code here
