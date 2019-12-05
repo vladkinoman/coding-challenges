8b73dd92b9e2792381a0903750a2797750922562
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-12-05 23:52:59 +0200

    Update: solution for the '063 Sorting: Bubble Sort' problem - add speedhacks

--- HackerRank/interview-preparation-kit/sorting/063-sorting-bubble-sort/c++/main.cpp
+++ HackerRank/interview-preparation-kit/sorting/063-sorting-bubble-sort/c++/main.cpp
@@ -8,14 +8,20 @@ vector<string> split_string(string);
 void countSwaps(vector<int> a) {
     int numberOfSwaps = 0;
     int n = a.size();
+    
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - 1; j++) {
-            // Swap adjacent elements if they are in decreasing order
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order.
             if (a[j] > a[j + 1]) {
                 swap(a[j], a[j + 1]);
                 numberOfSwaps++;
+                swapped = true;
             }
         }
+        // If no two elements were swapped by inner loop, then break.
+        if (swapped == false) 
+            break; 
     }
     cout << "Array is sorted in " << numberOfSwaps << " swaps." << endl;
     cout << "First Element: " << a[0] << endl;
