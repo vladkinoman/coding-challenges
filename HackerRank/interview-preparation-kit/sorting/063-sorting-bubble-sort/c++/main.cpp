6587248978a9d87c03a799d0b3378dd9db1a13cc
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-12-05 23:40:09 +0200

    Add solution to the '063 Sorting: Bubble Sort' problem of IPK

---
+++ HackerRank/interview-preparation-kit/sorting/063-sorting-bubble-sort/c++/main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the countSwaps function below.
+void countSwaps(vector<int> a) {
+    int numberOfSwaps = 0;
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            // Swap adjacent elements if they are in decreasing order
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+                numberOfSwaps++;
+            }
+        }
+    }
+    cout << "Array is sorted in " << numberOfSwaps << " swaps." << endl;
+    cout << "First Element: " << a[0] << endl;
+    cout << "Last Element: " << a[n - 1] << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (n < 2 || n > 600)
+        throw invalid_argument(" ");
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+        if (a_item < 1 || a_item > 2 * pow(10, 6))
+                throw invalid_argument(" ");
+        a[i] = a_item;
+    }
+
+    countSwaps(a);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
