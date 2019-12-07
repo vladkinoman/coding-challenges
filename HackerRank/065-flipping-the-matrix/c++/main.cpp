7c8ed5fac4ac961430c61b5c8760824cf6fcd278
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-12-07 23:40:49 +0200

    Add problem statement for the '065 Flipping the Matrix' problem

---
+++ HackerRank/065-flipping-the-matrix/c++/main.cpp
@@ -0,0 +1,43 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the flippingMatrix function below.
+int flippingMatrix(vector<vector<int>> matrix) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        vector<vector<int>> matrix(2*n);
+        for (int i = 0; i < 2*n; i++) {
+            matrix[i].resize(2*n);
+
+            for (int j = 0; j < 2*n; j++) {
+                cin >> matrix[i][j];
+            }
+
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+
+        int result = flippingMatrix(matrix);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
