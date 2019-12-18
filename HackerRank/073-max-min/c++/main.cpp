2eb26270b52db222456e3f3172e333ac1396e821
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-12-18 23:19:29 +0200

    Add '073 max min' problem to the ToDo list

---
+++ HackerRank/073-max-min/c++/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the maxMin function below.
+int maxMin(int k, vector<int> arr) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int k;
+    cin >> k;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item;
+        cin >> arr_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        arr[i] = arr_item;
+    }
+
+    int result = maxMin(k, arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
