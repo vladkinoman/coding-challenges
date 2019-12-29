81a9acd49b3abe6f6adb08bf4a856c7bf0735a51
Vlad Beklenyshchev <vladbeklenyshchev@gmail.com>
2019-12-29 22:13:43 +0200

    Add '082 Pseudo-Isomorphic substrings' problem to the ToDo list

---
+++ HackerRank/082-pseudo-isomorphic-substrings/c++/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the pseudoIsomorphicSubstrings function below.
+ */
+vector<int> pseudoIsomorphicSubstrings(string s) {
+    /*
+     * Write your code here.
+     */
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    vector<int> result = pseudoIsomorphicSubstrings(s);
+
+    for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+        fout << result[result_itr];
+
+        if (result_itr != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
