eee21855252ff42cbeac51d68e43bd79208bf5cf
vladkinoman <vladbeklenyshchev@gmail.com>
2020-10-19 02:11:09 +0300

    update for the problem 83: beat 26/33 tc (1 Wrong answer and 6 Timing)

--- HackerRank/083-two-characters/c++/main.cpp
+++ HackerRank/083-two-characters/c++/main.cpp
@@ -5,14 +5,30 @@ using namespace std;
 string ltrim(const string &);
 string rtrim(const string &);
 
+template <typename T1, typename T2>
+struct less_first {
+    typedef pair<T1, T2> type;
+    bool operator ()(type const& a, type const& b) const {
+        return a.second > b.second;
+    }
+};
+
 // Complete the alternate function below.
 int alternate(string s) {
-    /* 0. Use data structures */
+    /* 0. Concerning data structures */
     // We use map to store quantities of each character  
     unordered_map<char, int> quantities;
     for (char c : s) {
         quantities[c]++;
     }
+    // Now we need to sort the map. Unfortunately, map doesn't allow this to do
+    // So, we have to copy its elements to a vector and sort it
+    vector<pair<char, int> > mapcopy(quantities.begin(), quantities.end());
+    sort(mapcopy.begin(), mapcopy.end(), less_first<char, int>());
+    // Copy sorted elements back to the map
+    quantities.clear();
+    copy(mapcopy.begin(), mapcopy.end(), 
+          inserter(quantities, quantities.begin()));
     // We use doubly linked list to quickly remove consecutive values
     list<char> list_s;
     for (char c: s) {
@@ -20,49 +36,60 @@ int alternate(string s) {
     }
     /* 1. Go through the list and remove characters 
         that have adjacent identical values */
-    auto list_s_iter = list_s.begin();
-    char c = *list_s_iter;
-    while (++list_s_iter != list_s.end()) {
-        if (c == *list_s_iter) { 
+    auto list_s_it = list_s.begin();
+    char c = *list_s_it;
+    while (++list_s_it != list_s.end()) {
+        if (c == *list_s_it) { 
             list_s.remove(c);
             quantities.erase(c);
-            list_s_iter = list_s.begin();
+            list_s_it = list_s.begin();
         }
-        c = *list_s_iter;
+        c = *list_s_it;
     }
+    
     /* 2. Try to remove additional characters 
-        if an element has equal neighbors, we won't remove it*/
-    list_s_iter = list_s.begin();
+        if an element has equal neighbors, we won't remove it */
+    list_s_it = list_s.begin();
     auto list_s_last_it = prev(list_s.end(), 1);
     auto quantities_it = quantities.begin();
     while (quantities_it != quantities.end() && quantities.size() != 2) {
-        
-        while ((list_s_iter = find(++list_s_iter, list_s_last_it,
+
+        while ((list_s_it = find(++list_s_it, list_s_last_it,
          quantities_it->first)) != list_s_last_it 
-            && *prev(list_s_iter, 1) != *next(list_s_iter, 1))
+            && *prev(list_s_it, 1) != *next(list_s_it, 1))
             ;
 
-        if (list_s_iter == list_s_iter) {
+        if (list_s_it == list_s_last_it) {
             list_s.remove(quantities_it->first);
             quantities.erase(quantities_it->first);
-            list_s_iter = list_s.begin();
+            list_s_it = list_s.begin();
+            quantities_it = quantities.begin();
         } else {
             quantities_it++; 
         }
     }
 
-    // Debug
-    for (char c: list_s) {
-        cout << c << " ";
-    }
-    cout << "\n";
-
+    /* if there are only two characters in the map, return their amount */
     if (quantities.size() == 2) {
-        return quantities.begin()->second + next(quantities.begin(),1)->second;
+        return quantities.begin()->second +
+         next(quantities.begin(), 1)->second;
     }
 
     return 0;
 }
+
+/* Debug code 
+
+for (auto p: quantities) {
+    cout << p.first << " " << p.second << " ";
+}
+cout << "\n";
+
+for (char c: list_s) {
+    cout << c << " ";
+}
+cout << "\n";
+*/
 
 int main()
 {
