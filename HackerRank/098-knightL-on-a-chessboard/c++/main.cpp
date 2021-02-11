3ce34535a6cf1ea6115d911ce86c47144d66f0c0
vladkinoman <vladbeklenyshchev@gmail.com>
2021-02-11 23:59:38 +0200

    add 5 new problems on Complete Search

---
+++ HackerRank/098-knightL-on-a-chessboard/c++/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the knightlOnAChessboard function below.
+vector<vector<int>> knightlOnAChessboard(int n) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<int>> result = knightlOnAChessboard(n);
+
+    for (int i = 0; i < result.size(); i++) {
+        for (int j = 0; j < result[i].size(); j++) {
+            fout << result[i][j];
+
+            if (j != result[i].size() - 1) {
+                fout << " ";
+            }
+        }
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
---
+++ HackerRank/099-gridland-metro/c++/main.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the gridlandMetro function below.
+int gridlandMetro(int n, int m, int k, vector<vector<int>> track) {
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nmk_temp;
+    getline(cin, nmk_temp);
+
+    vector<string> nmk = split_string(nmk_temp);
+
+    int n = stoi(nmk[0]);
+
+    int m = stoi(nmk[1]);
+
+    int k = stoi(nmk[2]);
+
+    vector<vector<int>> track(k);
+    for (int i = 0; i < k; i++) {
+        track[i].resize(3);
+
+        for (int j = 0; j < 3; j++) {
+            cin >> track[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = gridlandMetro(n, m, k, track);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
