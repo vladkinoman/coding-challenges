df22c7415730b0ec7951d1b68759cf79889be442
vladkinoman <vladbeklenyshchev@gmail.com>
2021-02-21 23:42:25 +0200

    add solutions to the High Scores and Matrix problems (Exercism)

--- Exercism/python/high-scores/high_scores.py
+++ Exercism/python/high-scores/high_scores.py
@@ -1,10 +1,14 @@
 def latest(scores):
-    pass
-
+    if len(scores) > 0:
+        return scores[-1]
 
 def personal_best(scores):
-    pass
-
+    if len(scores) > 0:
+        best = 0
+        for score in scores:
+            if score > best:
+                best = score
+        return best
 
 def personal_top_three(scores):
-    pass
+    return sorted(scores, reverse=True)[:3]
--- Exercism/python/matrix/matrix.py
+++ Exercism/python/matrix/matrix.py
@@ -1,9 +1,13 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self._row = [[int(i_s) for i_s in r_s] for r_s in
+            [line_s.split(' ') for line_s in matrix_string.splitlines()]]
+
+        self._column = [[r[i] for r in self._row] for i in
+            range(0, len(self._row[0]))]
 
     def row(self, index):
-        pass
+        return self._row[index-1]
 
     def column(self, index):
-        pass
+        return self._column[index-1]
