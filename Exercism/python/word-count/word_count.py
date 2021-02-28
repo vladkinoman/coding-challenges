8119a9f747c8a65b1f43e22d5a4c4523288b146d
vladkinoman <vladbeklenyshchev@gmail.com>
2021-02-28 23:57:56 +0200

    add partial solution to the Word Count problem (1/3 - processing)

--- Exercism/python/word-count/word_count.py
+++ Exercism/python/word-count/word_count.py
@@ -1,14 +1,30 @@
-from curses.ascii import *
+from curses.ascii import isalnum
+from curses.ascii import isalpha
 
 def count_words(sentence):
+    n = len(sentence)
+    s = sentence[0:n]
+    #s.lower()
     result_dict = {}
-    word = ''
-    for i in range(0, len(sentence)):
-        if not ispunct(sentence[i]) and not isspace(sentence[i]) or sentence[i]=="'":
-            word += sentence[i]
-        elif word != '':
-            result_dict[word] = 0
-            word = ''
-    for key in result_dict.keys():
-            result_dict[key] = sentence.count(key)
-    return result_dict
+    # processing
+    i = 0
+    while i < n:
+        if not isalnum(s[i]) and s[i] != ' ' and s[i] != '\n' and s[i] != '\t':
+            if i == 0:
+                s = s[1:]
+            elif i == n-1:
+                s = s[0:i]
+            else:
+                if s[i] == '\'' and isalpha(s[i-1]) and isalpha(s[i+1]):
+                    # do nothing, this is an apostroph
+                    i += 1
+                else:
+                    s = s[:i] + s[i+1:]
+            n = len(s)
+        else: 
+            i += 1
+    # parsing
+    
+    # searching
+    
+    return s
