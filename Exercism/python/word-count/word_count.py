6904316193df66fc2d4d11717ec111801b40394c
vladkinoman <vladbeklenyshchev@gmail.com>
2021-03-01 18:02:22 +0200

    add solution to the Word Count problem (hard, it needs a rework)

--- Exercism/python/word-count/word_count.py
+++ Exercism/python/word-count/word_count.py
@@ -1,30 +1,37 @@
 from curses.ascii import isalnum
 from curses.ascii import isalpha
+from curses.ascii import isspace
 
 def count_words(sentence):
     n = len(sentence)
-    s = sentence[0:n]
-    #s.lower()
-    result_dict = {}
-    # processing
+    # parsing
+    s = sentence[0:n].lower()
     i = 0
     while i < n:
-        if not isalnum(s[i]) and s[i] != ' ' and s[i] != '\n' and s[i] != '\t':
+        if not isalnum(s[i]) and not isspace(s[i]):
             if i == 0:
-                s = s[1:]
+                s = s[i+1:]
             elif i == n-1:
                 s = s[0:i]
+            elif s[i] == '\'' and isalpha(s[i-1]) and isalpha(s[i+1]):
+                i += 1
             else:
-                if s[i] == '\'' and isalpha(s[i-1]) and isalpha(s[i+1]):
-                    # do nothing, this is an apostroph
-                    i += 1
-                else:
-                    s = s[:i] + s[i+1:]
-            n = len(s)
+                s = s[:i] + " " + s[i+1:]
+        elif isspace(s[i]):
+            if isspace(s[i-1]):
+                # there can only be one space between words
+                s = s[:i] + s[i+1:]
+            elif isalnum(s[i-1]) and s[i] != ' ' :
+                # this will make it easier to split the lines
+                s = s[:i] + " " + s[i+1:]
+            else:    
+                i += 1
         else: 
             i += 1
-    # parsing
-    
+        n = len(s)
+    parsed_l = s.strip().split(' ')
     # searching
-    
-    return s
+    counts = dict()
+    for i in parsed_l:
+        counts[i] = counts.get(i, 0) + 1
+    return counts
