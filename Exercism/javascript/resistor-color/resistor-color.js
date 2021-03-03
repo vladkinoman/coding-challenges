573b13a98d9c9fe47e03de1e5947d11c0b2ce8f5
vladkinoman <vladbeklenyshchev@gmail.com>
2021-03-03 22:51:42 +0200

    add solution to the Resistor Color problem (JS)

--- Exercism/javascript/resistor-color/resistor-color.js
+++ Exercism/javascript/resistor-color/resistor-color.js
@@ -1,10 +1,17 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const colorCode = () => {
-  throw new Error('Remove this statement and implement this function');
+export const colorCode = (color) => {
+  return COLORS.indexOf(color);
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+];
+
