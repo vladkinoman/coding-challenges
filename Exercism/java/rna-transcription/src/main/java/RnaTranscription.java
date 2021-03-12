d97ed831280f261b6508fab25f8bf5ded7a9a4b8
vladkinoman <vladbeklenyshchev@gmail.com>
2021-03-12 23:59:47 +0200

    add the RNA transcription problem to the backlog

---
+++ Exercism/java/rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,12 @@
+class RnaTranscription {
+
+    String transcribe(String dnaStrand) {
+        s = dnaStrand.clone();
+        s1 = dnaStrand.replace('G', 'C');
+        s2 = dnaStrand.replace('C', 'G');
+        s1 = .replace('T', 'A');
+        s2 = ;
+        return
+            .replace('A', 'U');
+    }
+}
