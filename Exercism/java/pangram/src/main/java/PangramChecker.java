9f996af63efb62a8cf11ad3476d3f2a680b50997
vladkinoman <vladbeklenyshchev@gmail.com>
2021-03-16 23:58:43 +0200

    add solution to the Pangram problem (Java)

--- Exercism/java/pangram/src/main/java/PangramChecker.java
+++ Exercism/java/pangram/src/main/java/PangramChecker.java
@@ -1,7 +1,33 @@
+import java.util.HashMap;
+
 public class PangramChecker {
+    private HashMap<Character, Integer> alphaOccurences;
+
+    public PangramChecker() {
+        alphaOccurences = new HashMap<>();
+        char c = 'a';
+        for (int i = 0; i < 26; i++, c++) {
+            alphaOccurences.put(c, 0);
+        }
+    }
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (input == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String s = new String(input);
+        s = s.toLowerCase();
+        System.out.println(s);        
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                alphaOccurences.replace(c, 1);
+            }
+        }
+
+        return !alphaOccurences.containsValue(0);
     }
 
 }
