fc261cd44b70e308adb1cf6d2b69d9ccfadf1ba6
vladkinoman <vladbeklenyshchev@gmail.com>
2021-03-20 22:05:09 +0200

    add solution to the Isogram problem (c lang)

--- Exercism/c/isogram/src/isogram.c
+++ Exercism/c/isogram/src/isogram.c
@@ -1,1 +1,19 @@
 #include "isogram.h"
+
+bool is_isogram(const char phrase[]) {
+    if (!phrase) return false;
+    int n = strlen(phrase);
+    char phrasecpy[n];
+    for (int i = 0; i < n; i++) {
+        phrasecpy[i] = tolower(phrase[i]);
+    }
+
+    int aoccurences[27] = {0};
+
+    for (int i = 0; i < n; i++) {
+        if (phrasecpy[i] == ' ' || phrasecpy[i] == '-') continue;
+        if (++aoccurences[phrasecpy[i] - 'a'] > 1) return false;
+    }
+
+    return true;
+}
--- Exercism/c/isogram/src/isogram.h
+++ Exercism/c/isogram/src/isogram.h
@@ -1,7 +1,9 @@
 #ifndef ISOGRAM_H
 #define ISOGRAM_H
-
 #include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 bool is_isogram(const char phrase[]);
 
