354d15b0a1eace0be941f2472ed0caa59244cd52
vladkinoman <vladbeklenyshchev@gmail.com>
2021-03-29 14:09:14 +0300

    add solution to the Grains pr. (C lang)

--- Exercism/c/grains/src/grains.c
+++ Exercism/c/grains/src/grains.c
@@ -1,1 +1,10 @@
 #include "grains.h"
+
+uint64_t square(uint8_t index) {
+    if (index <= 0 || index > 64) return 0;
+    return 1ull << (index - 1);
+}
+
+uint64_t total(void) {
+    return -1 | (1ull << 63); // -1 = ~1 + 1
+}
