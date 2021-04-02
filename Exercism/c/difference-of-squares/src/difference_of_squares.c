2741d0b068308f9f5d9a167da2bac9524a61f2f5
vladkinoman <vladbeklenyshchev@gmail.com>
2021-04-02 13:11:55 +0300

    add solution to the Difference of Squares pr. (C lang)

---
+++ Exercism/c/difference-of-squares/src/difference_of_squares.c
@@ -0,0 +1,15 @@
+#include "difference_of_squares.h"
+
+unsigned int sum_of_squares(unsigned int number) {
+    unsigned int sum = 0;
+    for (unsigned int i = 1; i <= number; i++) sum += i*i;
+    return sum;
+}
+unsigned int square_of_sum(unsigned int number) {
+    unsigned int sum = 0;
+    for (unsigned int i = 1; i <= number; i++) sum += i;
+    return sum * sum;
+}
+unsigned int difference_of_squares(unsigned int number) {
+    return square_of_sum(number) - sum_of_squares(number);
+}
