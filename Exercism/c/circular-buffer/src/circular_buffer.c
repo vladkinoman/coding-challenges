95fc8185722a99e573ad0c6fe1b3a115f7ab32dc
vladkinoman <vladbeklenyshchev@gmail.com>
2021-04-22 23:58:51 +0300

    add implementation of the new_buffer and delete_buffer funcs (C lang)

--- Exercism/c/circular-buffer/src/circular_buffer.c
+++ Exercism/c/circular-buffer/src/circular_buffer.c
@@ -1,1 +1,30 @@
 #include "circular_buffer.h"
+
+circular_buffer_t * new_circular_buffer(size_t capacity) {
+    circular_buffer_t * buffer = malloc(sizeof(circular_buffer_t));
+    buffer->values = malloc(sizeof(buffer_value_t) * capacity);
+    memset(buffer->values, 0, sizeof(buffer_value_t) * capacity);
+    return buffer;
+}
+
+int16_t read(circular_buffer_t* buffer, buffer_value_t* read_value) {
+    if (buffer->capacity == 0) return EXIT_FAILURE;
+    return 0;
+}
+
+int16_t write(circular_buffer_t* buffer, buffer_value_t new_value) {
+    return 0;
+}
+
+int16_t overwrite(circular_buffer_t* buffer, buffer_value_t new_value) {
+    return 0;
+}
+
+void clear_buffer(circular_buffer_t* buffer) {
+
+}
+
+void delete_buffer(circular_buffer_t* buffer) {
+    free(buffer->values);
+    free(buffer);
+}
--- Exercism/c/circular-buffer/src/circular_buffer.h
+++ Exercism/c/circular-buffer/src/circular_buffer.h
@@ -1,4 +1,22 @@
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
 
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef int16_t buffer_value_t;
+
+typedef struct { 
+    buffer_value_t * values;
+    int capacity;
+} circular_buffer_t;
+
+circular_buffer_t * new_circular_buffer(size_t capacity);
+int16_t read(circular_buffer_t* buffer, buffer_value_t* read_value);
+int16_t write(circular_buffer_t* buffer, buffer_value_t new_value);
+int16_t overwrite(circular_buffer_t* buffer, buffer_value_t new_value);
+void clear_buffer(circular_buffer_t* buffer);
+void delete_buffer(circular_buffer_t* buffer);
+
 #endif
